b10715dcb0ff9ea5e20ee6534ca66170879863dd
세훈 <jsh9435@gmail.com>
2024-09-03 15:25:44 +0900

    회원가입 페이지

---
+++ front2/project/src/components/Modify.jsx
@@ -0,0 +1,15 @@
+import React, {useRef, useState, useEffect} from 'react'
+import instance from '../axios'
+
+const Modify = () => {
+
+
+
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default Modify
