dad33ca1abc380a57cf3e97e704c2ad553455f2f
dj <work.99.da@gamil.com>
2024-09-04 17:33:24 +0900

    프론트확인

--- front2/project/src/components/Community.jsx
+++ front2/project/src/components/Community.jsx
@@ -1,9 +1,79 @@
 import React from 'react'
+import { useEffect, useState } from 'react'
+// import { Tab } from '@headlessui/react';
+// import { User, Calendar, MessageSquare, Heart } from 'lucide-react';
+// import axios from 'axios';
+// import { response } from 'express';
+
+
+
+const posts = [
+    { id: 1, title: "오늘 날씨가 좋네요", content: "날씨가 정말 좋아요!", category: "자유게시글" },
+    { id: 2, title: "제 토마토 좀 보세요", content: "토마토가 잘 자랐어요.", category: "직접키운식물자랑" },
+    { id: 3, title: "간단한 샐러드 레시피", content: "맛있는 샐러드 만드는 법", category: "레시피공유" },
+    { id: 4, title: "주말 계획이 있나요?", content: "저는 영화 볼 예정이에요.", category: "자유게시글" },
+    { id: 5, title: "제 다육이를 소개합니다", content: "귀여운 다육이에요.", category: "직접키운식물자랑" },
+    { id: 6, title: "쉬운 파스타 레시피", content: "누구나 할 수 있는 파스타 요리", category: "레시피공유" },
+  ]
+
+  const categories = ['전체', '자유게시글', '직접키운식물자랑', '레시피공유']
 
 const Community = () => {
+
+    const [selectedCategory, setSelectedCategory] = useState('전체')
+
+    const filteredPosts = selectedCategory === '전체'
+        ? posts
+        : posts.filter(post => post.category === selectedCategory)
+   
+
   return (
-    <div>Community</div>
+    <div className="container">
+         <h1 className="title">Community!</h1>
+            <div>
+                {categories.map(category => (
+                    <button
+                    key={category}
+                    onClick={() => setSelectedCategory(category)}
+                    className=''
+                    >
+                    {category}
+                    </button>
+                ))}
+            </div>
+       
+            <ul>
+                {filteredPosts.map((post) => (
+                    <li key={post.id} className=''>
+                        {post.imageUrl && (
+                            <img 
+                            src={post.imageUrl} 
+                            alt={`${post.title}의 이미지`} 
+                            className=''
+                            />
+                        )}
+                        <h2 className=''>{post.title}</h2>
+                        <p className=''>{post.category}</p>
+                        <p className=''>{post.content}</p>
+                    </li>
+                ))}
+            </ul>
+    </div>   
   )
 }
 
+
+
 export default Community
+
+
+
+       
+  
+      
+
+
+
+
+
+ 
